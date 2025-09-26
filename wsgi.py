0ec09212afd313b53eaf269fd178701228a9a444
Parth Acharya <125336330+Projectworkpls@users.noreply.github.com>
2025-09-26 08:11:06 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,15 +1,3 @@
-import os
-import sys
+from app import create_app
 
-# Ensure the project directory is on the Python path
-sys.path.insert(0, os.path.dirname(__file__))
-
-# Import the create_app function from main.py
-from main import create_app
-
-# Create the Flask app instance
 app = create_app()
-
-# Optional: run locally with `python wsgi.py`
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
